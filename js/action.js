822a38ef671841a83e65eb19589c29d307fb9682
RainL <boomshakalakayu@gmail.com>
2020-02-15 18:41:54 +0800

    about, contact page

---
+++ js/action.js
@@ -0,0 +1,5 @@
+$(document).ready(function(e){
+	$('.menu').click(function(e){
+		$('#header').find('.nav').toggleClass('active');
+	})
+})	
---
+++ js/carousel.js
@@ -0,0 +1,6 @@
+$(document).ready(function(e){
+	$('.slide').slick({
+		autoplay: true,
+		dots: true,
+	});
+})	
