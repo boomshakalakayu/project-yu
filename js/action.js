b67cf980e1fbc84ed5b477324764e5a64655c373
RainL <boomshakalakayu@gmail.com>
2020-03-28 17:55:10 +0800

    product page

--- js/action.js
+++ js/action.js
@@ -1,5 +1,44 @@
 $(document).ready(function(e){
+
+	// nav menu //
+
 	$('.menu').click(function(e){
 		$('#header').find('.nav').toggleClass('active');
 	})
-})	
+
+	// slider range bar //
+
+	$('.slider-range').slider({
+		range: true,
+        min: 0,
+        max: 800,
+        values: [ 10, 599 ],
+        slide: function( event, ui ) {
+          $( "#amount" ).val( "Price $" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        }
+	})
+
+	$( "#amount" ).val( "Price $" + $( ".slider-range" ).slider( "values", 0 ) +
+	      " - $" + $( ".slider-range" ).slider( "values", 1 ) )
+
+	// side bar menu //
+
+	$('.filter-option').click(function(e){
+		$('.filter').addClass('show');
+		$('.bg-cover').fadeIn();
+	})
+
+	$('.categories-option').click(function(e){
+		$('.categories').addClass('show');
+		$('.bg-cover').fadeIn();
+	})
+
+	$('.close-menu').click(function(e){
+		$('.filter, .categories').removeClass('show');
+		$('.bg-cover').fadeOut();
+	})
+
+	$('.bg-cover').click(function(e){
+		$('.close-menu').click();
+	})
+});
--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,25 @@
 $(document).ready(function(e){
-	$('.slide').slick({
+	// workspace carousel
+
+	$('.aboutSlide').slick({
 		autoplay: true,
 		dots: true,
 	});
+
+	// product carousel
+
+	$('.product-slide').slick({
+		slidesToShow: 1,
+	    slidesToScroll: 1,
+	    arrows: false,
+	    fade: true,
+	    asNavFor: '.product-nav'
+	});
+	$('.product-nav').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		asNavFor: '.product-slide',
+		centerMode: true,
+		focusOnSelect: true
+	})
 })	
